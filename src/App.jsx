95eb53b469230eed19a46ee1d4e728b86a17425e
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-09-21 20:08:27 +0600

    Setup React Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/header"
+
+function App() {
+
+
+  return (
+    <>
+     
+    <Header></Header>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-center text-3xl font-bold color[#1C1B1B]'>Course Registration</h1>
+        </div>
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
