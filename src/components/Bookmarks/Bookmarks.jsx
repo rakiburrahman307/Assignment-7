b66c3b71b422ea31138abe2be102f37b02a826e9
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-09-23 21:58:11 +0600

    calculation section added

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -7,29 +7,29 @@ const Bookmarks = ({ bookmarks }) => {
   const notify = () => {
     toast("You Do Not Have More Credit");
   };
+           let count = 0;
+           let remaining = 20;
+           let credit = 0;
 
-  let count = 0;
-  let remaining = 20;
-  let credit = 0;
+    bookmarks.forEach(element => {
+        if (remaining <= 0) {
+            return;
+          }
+      
+          if (remaining < element.credit) {
+            notify();
+            return;
+          }
+            credit += element.credit;
+           count += element.price;
+           remaining -= element.credit;
+    });
 
-  bookmarks.forEach((element) => {
-    if (remaining <= 0) {
-      return;
-    }
-
-    if (remaining < element.credit) {
-      notify();
-      return;
-    }
-
-    count += element.price;
-    remaining -= element.credit;
-    credit += element.credit;
-  });
+           
 
   return (
     <div className='md:h-1/3'>
-      <ToastContainer />
+      <ToastContainer/>
       <div className='border-b-2 mb-4'>
         <h2 className='text-xl font-bold text-blue-400 mb-4'>
           Credit Hour Remaining {remaining} hr{' '}
@@ -38,9 +38,16 @@ const Bookmarks = ({ bookmarks }) => {
       <div className='border-b-2'>
         <h2 className='text-xl font-bold text-black'>Course Name</h2>
         <ol style={{ listStyleType: 'number' }} className='ml-6 mb-4'>
-          {bookmarks.map((bookmark, idx) => (
-            <Bookmark key={idx} bookmark={bookmark}></Bookmark>
-          ))}
+          { 
+          bookmarks.map((bookmark, idx) => {
+            if (credit>=20) {
+             return notify();   
+            }
+           return <Bookmark key={idx} bookmark={bookmark}></Bookmark>
+          }
+            
+          )
+          }
         </ol>
       </div>
       <div className='border-b-2 mb-2 mt-2'>
